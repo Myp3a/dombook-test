4bf2d1dac490b6a169e7381f38f571ed21dbd104
Myp3a <murza8965@gmail.com>
2021-08-07 00:08:46 +0500

    dombook test

---
+++ tests/test.js
@@ -0,0 +1,57 @@
+const { test, expect } = require('@playwright/test');
+
+const trace_api_requests = true;
+
+test('dombook', async ({page}) => {  
+  //const browser = await chromium.launch();  
+  //const page = await browser.newPage();
+  var sales_top;
+
+  expect.extend({
+    apiResponse(received, code) {
+      if (received.status() == code) {
+        return {
+          message: () => `Expected status ${code}, received ${received.status()} on ${received.url()}`,
+          pass: true
+        };
+      } else {
+        return {
+          message: () => `Expected status ${code}, received ${received.status()} on ${received.url()}`,
+          pass: false
+        };
+      }
+    }
+  });
+
+  page.on('request', async (request) => {
+    //console.log('>>', request.method(), request.url());
+    if (request.url().includes('https://api.dombook.ru/api/')) {
+      if (trace_api_requests) {
+        console.log('>>', request.method(), request.url());
+      }
+    }
+    });
+  page.on('response', async (response) => {
+    //console.log('<<', response.status(), response.url())); 
+    if (response.url().includes('https://api.dombook.ru/api/')) {
+      if (trace_api_requests) {
+        console.log('>>', response.status(), response.url());
+      }
+      if (response.url() == 'https://api.dombook.ru/api/project/top/saleCount' && typeof sales_top === 'undefined') {
+        sales_top = await response.json()
+      }
+      expect(response).apiResponse(200)
+        //console.log(response.url(), 'failed with code', response.status())
+      }
+    }); 
+
+  await page.goto('https://dombook.ru/');
+  for (const house of sales_top) {
+    await page.goto(`https://dombook.ru/project/${house.id}`);
+    await page.goBack();
+  }
+  await page.goto('https://dombook.ru/');
+
+
+  await browser.close();
+});
